10cdcc66623668e5c7c08a102e22cda48cd9e7df
kaihung <s2523154@hotmail.com>
2022-03-16 16:23:56 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # smtpcli
 smtp client tool to make sending emails easier.
+
+## Basic Sample
+``` go
+package main
+
+import (
+	"fmt"
+	"github.com/ka1hung/smtpcli"
+)
+
+func main() {
+	sender := smtpcli.NewServer("smtp.gmail.com", 587, "user@gmail.com", "password")
+	m := smtpcli.NewMessage("Subject", "Body message.")
+    m.To = []string{"user1@gmail.com", "user2@gmail.com"}
+	fmt.Println(sender.Send(m))
+}
+```
+
+## Sample with cc bcc
+``` go
+package main
+
+import (
+	"fmt"
+	"github.com/ka1hung/smtpcli"
+)
+
+func main() {
+	sender := smtpcli.NewServer("smtp.gmail.com", 587, "user@gmail.com", "password")
+	m := smtpcli.NewMessage("Subject", "Body message.")
+    m.To = []string{"user1@gmail.com", "user2@gmail.com"}
+	m.CC = []string{"user3@gmail.com", "user4@gmail.com"}
+	m.BCC = []string{"bc@gmail.com"}
+	fmt.Println(sender.Send(m))
+}
+```
+
+## Sample to modify content type
+``` go
+package main
+
+import (
+	"fmt"
+	"github.com/ka1hung/smtpcli"
+)
+
+func main() {
+	sender := smtpcli.NewServer("smtp.gmail.com", 587, "user@gmail.com", "password")
+	m := smtpcli.NewMessage("Subject", "<h1>Body message</h1>")
+    m.To = []string{"user1@gmail.com", "user2@gmail.com"}
+	m.ContentType = "text/html; charset=utf-8"
+	fmt.Println(sender.Send(m))
+}
+```
+
+## Sample to attach files 
+``` go
+package main
+
+import (
+	"fmt"
+	"github.com/ka1hung/smtpcli"
+)
+
+func main() {
+	sender := smtpcli.NewServer("smtp.gmail.com", 587, "user@gmail.com", "password")
+	m := smtpcli.NewMessage("Subject", "<h1>Body message</h1>")
+    m.To = []string{"user1@gmail.com", "user2@gmail.com"}
+	m.ContentType = "text/html; charset=utf-8"
+    m.AttachFiles([]string{"./123.txt", "./abc.png", "./aaa.svg"})
+	fmt.Println(sender.Send(m))
+}
+```
+
+improve form the gist https://gist.github.com/douglasmakey/90753ecf37ac10c25873825097f46300
---
+++ sender.go
@@ -0,0 +1,109 @@
+package smtpcli
+
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"net/smtp"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+type Sender struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	auth     smtp.Auth
+}
+
+type Message struct {
+	ContentType string
+	To          []string
+	CC          []string
+	BCC         []string
+	Subject     string
+	Body        string
+	Attachments map[string][]byte
+}
+
+// port=> 578:Use TLS; 25:Non-encryption
+func NewServer(addr string, port int, user, password string) *Sender {
+	auth := smtp.PlainAuth("", user, password, addr)
+	return &Sender{
+		Host:     addr + ":" + strconv.Itoa(port),
+		Port:     port,
+		User:     user,
+		Password: password,
+		auth:     auth}
+}
+
+func (s *Sender) Send(m *Message) error {
+	return smtp.SendMail(s.Host, s.auth, s.User, m.To, m.toBytes())
+}
+
+// default set ContentType to text/plain; charset=UTF-8.
+// body: use /r/n to make newline.
+func NewMessage(subject, body string) *Message {
+	return &Message{ContentType: "text/plain; charset=UTF-8", Subject: subject, Body: body, Attachments: make(map[string][]byte)}
+}
+
+// fs: set the files path
+func (m *Message) AttachFiles(files []string) error {
+	for _, f := range files {
+		b, err := ioutil.ReadFile(f)
+		if err != nil {
+			return err
+		}
+
+		_, fileName := filepath.Split(f)
+		m.Attachments[fileName] = b
+	}
+	return nil
+}
+
+func (m *Message) toBytes() []byte {
+	buf := bytes.NewBuffer(nil)
+	withAttachments := len(m.Attachments) > 0
+	buf.WriteString(fmt.Sprintf("Subject: %s\n", m.Subject))
+	buf.WriteString(fmt.Sprintf("To: %s\n", strings.Join(m.To, ",")))
+	if len(m.CC) > 0 {
+		buf.WriteString(fmt.Sprintf("Cc: %s\n", strings.Join(m.CC, ",")))
+	}
+
+	if len(m.BCC) > 0 {
+		buf.WriteString(fmt.Sprintf("Bcc: %s\n", strings.Join(m.BCC, ",")))
+	}
+
+	buf.WriteString("MIME-Version: 1.0\n")
+	writer := multipart.NewWriter(buf)
+	boundary := writer.Boundary()
+	if withAttachments {
+		buf.WriteString(fmt.Sprintf("Content-Type: multipart/mixed; boundary=%s\n", boundary))
+		buf.WriteString(fmt.Sprintf("--%s\n", boundary))
+	}
+	buf.WriteString(fmt.Sprintf("Content-Type: %s\n", m.ContentType))
+
+	buf.WriteString(m.Body)
+	if withAttachments {
+		for k, v := range m.Attachments {
+			buf.WriteString(fmt.Sprintf("\n\n--%s\n", boundary))
+			buf.WriteString(fmt.Sprintf("Content-Type: %s\n", http.DetectContentType(v)))
+			buf.WriteString("Content-Transfer-Encoding: base64\n")
+			buf.WriteString(fmt.Sprintf("Content-Disposition: attachment; filename=%s\n", k))
+
+			b := make([]byte, base64.StdEncoding.EncodedLen(len(v)))
+			base64.StdEncoding.Encode(b, v)
+			buf.Write(b)
+			buf.WriteString(fmt.Sprintf("\n--%s", boundary))
+		}
+
+		buf.WriteString("--")
+	}
+
+	return buf.Bytes()
+}
